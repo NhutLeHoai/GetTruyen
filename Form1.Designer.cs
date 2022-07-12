fb46007f7ee5eca0740efb4ceadbce3ae7789619
NhutLeHoai <theghostloved@gmail.com>
2022-07-12 23:43:46 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace GetTruyen
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.txbUrl = new System.Windows.Forms.TextBox();
+            this.grpUrl = new System.Windows.Forms.GroupBox();
+            this.btnGetList = new System.Windows.Forms.Button();
+            this.btnDownAll = new System.Windows.Forms.Button();
+            this.btnDownByChapter = new System.Windows.Forms.Button();
+            this.ckdChapterList = new System.Windows.Forms.CheckedListBox();
+            this.fbdDirPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.txbStatus = new System.Windows.Forms.TextBox();
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.ckdSelectAll = new System.Windows.Forms.CheckBox();
+            this.grpUrl.SuspendLayout();
+            this.pnl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txbUrl
+            // 
+            this.txbUrl.Location = new System.Drawing.Point(6, 19);
+            this.txbUrl.Name = "txbUrl";
+            this.txbUrl.Size = new System.Drawing.Size(468, 20);
+            this.txbUrl.TabIndex = 0;
+            // 
+            // grpUrl
+            // 
+            this.grpUrl.Controls.Add(this.txbUrl);
+            this.grpUrl.Location = new System.Drawing.Point(13, 12);
+            this.grpUrl.Name = "grpUrl";
+            this.grpUrl.Size = new System.Drawing.Size(481, 50);
+            this.grpUrl.TabIndex = 1;
+            this.grpUrl.TabStop = false;
+            this.grpUrl.Text = "Nhập Url";
+            // 
+            // btnGetList
+            // 
+            this.btnGetList.Location = new System.Drawing.Point(12, 68);
+            this.btnGetList.Name = "btnGetList";
+            this.btnGetList.Size = new System.Drawing.Size(100, 23);
+            this.btnGetList.TabIndex = 1;
+            this.btnGetList.Text = "Lấy danh sách truyện";
+            this.btnGetList.UseVisualStyleBackColor = true;
+            this.btnGetList.Click += new System.EventHandler(this.btnGetList_Click);
+            // 
+            // btnDownAll
+            // 
+            this.btnDownAll.Location = new System.Drawing.Point(3, 2);
+            this.btnDownAll.Name = "btnDownAll";
+            this.btnDownAll.Size = new System.Drawing.Size(100, 23);
+            this.btnDownAll.TabIndex = 2;
+            this.btnDownAll.Text = "Tải hết";
+            this.btnDownAll.UseVisualStyleBackColor = true;
+            this.btnDownAll.Click += new System.EventHandler(this.btnDownAll_Click);
+            // 
+            // btnDownByChapter
+            // 
+            this.btnDownByChapter.Location = new System.Drawing.Point(109, 2);
+            this.btnDownByChapter.Name = "btnDownByChapter";
+            this.btnDownByChapter.Size = new System.Drawing.Size(100, 23);
+            this.btnDownByChapter.TabIndex = 3;
+            this.btnDownByChapter.Text = "Tải Theo Chap";
+            this.btnDownByChapter.UseVisualStyleBackColor = true;
+            this.btnDownByChapter.Click += new System.EventHandler(this.btnDownByChapter_Click);
+            // 
+            // ckdChapterList
+            // 
+            this.ckdChapterList.CheckOnClick = true;
+            this.ckdChapterList.FormattingEnabled = true;
+            this.ckdChapterList.Location = new System.Drawing.Point(13, 117);
+            this.ckdChapterList.Name = "ckdChapterList";
+            this.ckdChapterList.Size = new System.Drawing.Size(474, 514);
+            this.ckdChapterList.TabIndex = 5;
+            // 
+            // fbdDirPath
+            // 
+            this.fbdDirPath.Description = "Chọn đường dẫn đến nơi bạn muốn lưu truyện";
+            // 
+            // txbStatus
+            // 
+            this.txbStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.txbStatus.Enabled = false;
+            this.txbStatus.Location = new System.Drawing.Point(337, 69);
+            this.txbStatus.Name = "txbStatus";
+            this.txbStatus.Size = new System.Drawing.Size(150, 20);
+            this.txbStatus.TabIndex = 9;
+            this.txbStatus.Text = "Đang chờ tải...";
+            this.txbStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pnl1
+            // 
+            this.pnl1.Controls.Add(this.btnDownAll);
+            this.pnl1.Controls.Add(this.btnDownByChapter);
+            this.pnl1.Enabled = false;
+            this.pnl1.Location = new System.Drawing.Point(116, 64);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(215, 29);
+            this.pnl1.TabIndex = 10;
+            // 
+            // ckdSelectAll
+            // 
+            this.ckdSelectAll.AutoSize = true;
+            this.ckdSelectAll.Enabled = false;
+            this.ckdSelectAll.Location = new System.Drawing.Point(16, 96);
+            this.ckdSelectAll.Name = "ckdSelectAll";
+            this.ckdSelectAll.Size = new System.Drawing.Size(69, 17);
+            this.ckdSelectAll.TabIndex = 4;
+            this.ckdSelectAll.Text = "Select all";
+            this.ckdSelectAll.UseVisualStyleBackColor = true;
+            this.ckdSelectAll.CheckedChanged += new System.EventHandler(this.ckdSelectAll_CheckedChanged);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 644);
+            this.Controls.Add(this.ckdSelectAll);
+            this.Controls.Add(this.pnl1);
+            this.Controls.Add(this.txbStatus);
+            this.Controls.Add(this.ckdChapterList);
+            this.Controls.Add(this.btnGetList);
+            this.Controls.Add(this.grpUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get Truyện By NhutLe";
+            this.grpUrl.ResumeLayout(false);
+            this.grpUrl.PerformLayout();
+            this.pnl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbUrl;
+        private System.Windows.Forms.GroupBox grpUrl;
+        private System.Windows.Forms.Button btnGetList;
+        private System.Windows.Forms.Button btnDownAll;
+        private System.Windows.Forms.Button btnDownByChapter;
+        private System.Windows.Forms.CheckedListBox ckdChapterList;
+        private System.Windows.Forms.FolderBrowserDialog fbdDirPath;
+        private System.Windows.Forms.TextBox txbStatus;
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.CheckBox ckdSelectAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using xNet;
+
+namespace GetTruyen
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        #region define some public variable
+        Dictionary<string, string> chapterList = new Dictionary<string, string>();
+        string mangaName = "";
+        string webKey = "";
+        ArrayList defWeb = new ArrayList();
+        #endregion
+
+        class Request
+        {
+            internal static HttpRequest request;
+        } //define new request
+
+        #region Remove special character in manga name & chapter name
+        string specialCharProcess(string mangaName)
+        {
+            string[] specialChar = new string[] { "\\", "/", ":", "*", "?", "\"", "<", ">", "|" };
+            for (int i = 0; i < specialChar.Length; i++)
+            {
+                mangaName = mangaName.Replace(specialChar[i], "");
+            }
+            return mangaName;
+        }
+        #endregion
+
+        #region Get chapter url
+        void GetNettruyenChapterUrl()
+        {
+            chapterList.Clear();
+            string url = txbUrl.Text;
+            HtmlWeb web = new HtmlWeb();
+            try {
+                HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+
+                string xPath = "";
+                string xPathTitle = "";
+
+                foreach (var subUrl in url.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    defWeb.Add(subUrl);
+                }
+                if (url.Contains("nettruyen"))
+                {
+                    xPath = "//div[@class='list-chapter']//a[@href]";
+                    xPathTitle = "//*[@id='item-detail']/h1";
+                    webKey = "nettruyen";
+                }
+                else if (url.Contains("truyenqq"))
+                {
+                    xPath = "//div[@class='list_chapter']//a[@href]";
+                    xPathTitle = "/html/body/div[1]/div[1]/div[3]/div/div[1]/div[2]/h1";
+                    webKey = "truyenqq";
+                }
+                else
+                {
+                    MessageBox.Show("Hiện tại chỉ hỗ trợ Nettruyen và TruyenQQ", "Trang web không hỗ trợ");
+                }
+                try
+                {
+                    foreach (HtmlNode link in doc.DocumentNode.SelectNodes(xPath))
+                    {
+                        string chapterName = specialCharProcess(link.InnerHtml.Replace("Chương", "Chapter"));
+                        HtmlAttribute chapterLink = link.Attributes["href"];
+                        if((chapterLink.Value != "#"))
+                        {
+                            chapterList.Add(chapterName, chapterLink.Value);
+                        }
+                            
+                    }
+                    HtmlNode title = doc.DocumentNode.SelectSingleNode(xPathTitle);
+                    mangaName = title.InnerText;
+                    ckdChapterList.Items.Clear();
+                    foreach (string chapterName in chapterList.Keys)
+                    {
+                        ckdChapterList.Items.Add(chapterName);
+                    }
+                    pnl1.Enabled = true;
+                    ckdSelectAll.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Đường dẫn không hợp lệ, vui lòng copy url ở mục chọn chapter", "Lỗi đường dẫn");
+                }
+            }
+            
+
+            catch { MessageBox.Show("Đường dẫn phải bao gồm phần http://\nVí dụ: http://www.nettruyenco.com/truyen-tranh/naruto-cuu-vi-ho-ly-11996", "Đường dẫn url không hợp lệ!"); }
+
+        }
+        #endregion
+
+        #region Download images
+        void ImagesDownloader(string url,string fileName)
+        {
+            
+            Request.request = new HttpRequest();
+            Request.request.Cookies = new CookieDictionary();
+            string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.49";
+            string referer = "www" + defWeb[1];
+            Request.request.ConnectTimeout = 5000;
+            Request.request.AddHeader("User-Agent", userAgent);
+            Request.request.AddHeader("Referer",referer);
+            if (!url.Contains(@"https:") && (!url.Contains(@"http:")))
+            {
+                url = @"http:" + url;
+            }
+            var img = Request.request.Get(url).ToMemoryStream().ToArray();
+            File.WriteAllBytes(fileName+".jpg", img);
+            Request.request.Close();
+            
+        }
+        #endregion
+
+        #region Get img Url
+        void GetImgUrl(string chapterUrl,string chapterDirPath)
+        {
+            HtmlWeb chapterWeb = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument chapterNode = chapterWeb.Load(chapterUrl);
+            string imgXPath = "";
+            if (webKey == "nettruyen")
+            {
+                imgXPath = "//div[@class='page-chapter']//img[@src]";
+            }
+            else
+            {
+                imgXPath = "//div[@class ='chapter_content']//img[@src]";
+            }
+            int i = 1;
+            foreach (HtmlNode imgNode in chapterNode.DocumentNode.SelectNodes(imgXPath))
+            {
+                HtmlAttribute imgLink = imgNode.Attributes["src"];
+                HtmlAttribute imgIndex = imgNode.Attributes["data-index"];
+                string _chapterDirPath = chapterDirPath+"\\"+i.ToString();
+                ImagesDownloader(imgLink.Value, _chapterDirPath);
+                i++;
+            }
+
+        }
+        
+        void GetImgUrlAll (string request,string mangaDirPath)
+        {
+            
+            
+            Directory.CreateDirectory(mangaDirPath);
+            if (request == "downall")
+            {
+                foreach (var chapter in chapterList.Keys)
+                {
+                    string chapterName = specialCharProcess(chapter);
+                    txbStatus.Text = "Đang tải: " + chapterName;
+                    string chapterDirPath = mangaDirPath +"\\"+ chapterName;
+                    Directory.CreateDirectory(chapterDirPath);
+                    GetImgUrl(chapterList[chapter],chapterDirPath);
+                       
+                }
+            }
+            if (request == "downbychapter")
+            {
+                for (int i = 0; i < chapterList.Count; i++)
+                {
+                    if (ckdChapterList.GetItemChecked(i))
+                    {
+                        string selectedChapter = specialCharProcess(ckdChapterList.Items[i].ToString());
+                        txbStatus.Text = "Đang tải: " + selectedChapter;
+                        string chapterDirPath = mangaDirPath + "\\" + selectedChapter;
+                        Directory.CreateDirectory(chapterDirPath);
+                        GetImgUrl(chapterList[selectedChapter], chapterDirPath);
+                        
+                    }
+                }
+            }
+            txbStatus.Text = "Tải hoàn tất!";
+            MessageBox.Show("Quá trình tải truyện hoàn tất!");
+
+            
+        }
+        #endregion
+
+        private void btnGetList_Click(object sender, EventArgs e)
+        {
+            Thread getChapterUrl = new Thread(() => { GetNettruyenChapterUrl(); });
+            getChapterUrl.Start();
+            
+        } //Click and get Get list URL
+
+        private void btnDownAll_Click(object sender, EventArgs e)
+        {
+            
+            if (fbdDirPath.ShowDialog() == DialogResult.OK)
+            {
+                mangaName = specialCharProcess(mangaName);
+                string mangaDirPath = fbdDirPath.SelectedPath + "\\" + mangaName;
+                Thread downAll = new Thread(() => { GetImgUrlAll("downall",mangaDirPath); });
+                downAll.IsBackground = true;
+                downAll.Start();
+            }
+        } // Download all 
+
+        private void btnDownByChapter_Click(object sender, EventArgs e)
+        {
+            if (fbdDirPath.ShowDialog() == DialogResult.OK)
+            {
+                mangaName = specialCharProcess(mangaName);
+                string mangaDirPath = fbdDirPath.SelectedPath + "\\" + mangaName;
+                Thread downByChapter = new Thread(() => { GetImgUrlAll("downbychapter",mangaDirPath); });
+                downByChapter.IsBackground = true;
+                downByChapter.Start();
+            }
+        } //Click and download by chapter
+
+        private void ckdSelectAll_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ckdSelectAll.Text == "Select all")
+            {
+                ckdSelectAll.Text = "Deselect all";
+                for (int i = 0; i < chapterList.Count; i++)
+                {
+                    ckdChapterList.SetItemChecked(i, true);
+                }
+            }
+            else
+            {
+                ckdSelectAll.Text = "Select all";
+                for (int i = 0; i < chapterList.Count; i++)
+                {
+                    ckdChapterList.SetItemChecked(i, false);
+                }
+            }
+            
+        } //select - deselect all chapter list
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
